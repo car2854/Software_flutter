83cc4ede29607feecf14a26738577035af0cc7bc
car2854 <39568391+car2854@users.noreply.github.com>
2020-12-10 23:20:11 -0400

    Agregando una API para la camara

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_software/src/export/routes.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
